6505c1055ce04593f7dd45208a057b42b40c34b7
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-08 18:53:24 +0300

    dev: remove stale USB restrictions from incremental images

--- br2-external/board/raspberrypi4-64/post-build.sh
+++ br2-external/board/raspberrypi4-64/post-build.sh
@@ -5,6 +5,7 @@ TARGET_DIR="$1"
 
 # Remove obsolete files that Buildroot overlays do not delete during incremental builds.
 rm -f "$TARGET_DIR/etc/udev/rules.d/10-proaudio-usb-storage-only.rules"
+rm -f "$TARGET_DIR/etc/udev/rules.d/10-proaudio-usb-allowlist.rules"
 rm -f "$TARGET_DIR/usr/lib/systemd/system/proaudio-player-web.service"
 rm -f "$TARGET_DIR/etc/systemd/system/multi-user.target.wants/proaudio-player-web.service"
 rm -f "$TARGET_DIR/usr/bin/proaudio-player-web"
