eee8a09ba459fd526e55d7ab54681fcf5fea8113
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-08 18:34:48 +0300

    feat(dev): build spotifyd with MPRIS support

--- br2-external/package/proaudio-spotifyd/proaudio-spotifyd.mk
+++ br2-external/package/proaudio-spotifyd/proaudio-spotifyd.mk
@@ -12,9 +12,9 @@ PROAUDIO_SPOTIFYD_SITE = https://static.crates.io/crates/spotifyd
 PROAUDIO_SPOTIFYD_DL_SUBDIR = spotifyd
 PROAUDIO_SPOTIFYD_LICENSE = GPL-3.0-only
 PROAUDIO_SPOTIFYD_LICENSE_FILES = LICENSE
-PROAUDIO_SPOTIFYD_DEPENDENCIES = host-pkgconf openssl pulseaudio
-PROAUDIO_SPOTIFYD_CARGO_BUILD_OPTS = --no-default-features --features pulseaudio_backend
-PROAUDIO_SPOTIFYD_CARGO_INSTALL_OPTS = --no-default-features --features pulseaudio_backend
+PROAUDIO_SPOTIFYD_DEPENDENCIES = dbus host-pkgconf openssl pulseaudio
+PROAUDIO_SPOTIFYD_CARGO_BUILD_OPTS = --no-default-features --features pulseaudio_backend,dbus_mpris
+PROAUDIO_SPOTIFYD_CARGO_INSTALL_OPTS = --no-default-features --features pulseaudio_backend,dbus_mpris
 
 define PROAUDIO_SPOTIFYD_EXTRACT_CMDS
 	$(TAR) -C $(@D) --strip-components=1 -xf \
