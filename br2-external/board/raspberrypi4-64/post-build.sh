d1f7c5f34107c34c9aa1a88b61e9c422cfc97926
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-08 18:53:54 +0300

    dev: ensure SSH is enabled at boot

--- br2-external/board/raspberrypi4-64/post-build.sh
+++ br2-external/board/raspberrypi4-64/post-build.sh
@@ -9,3 +9,10 @@ rm -f "$TARGET_DIR/etc/udev/rules.d/10-proaudio-usb-allowlist.rules"
 rm -f "$TARGET_DIR/usr/lib/systemd/system/proaudio-player-web.service"
 rm -f "$TARGET_DIR/etc/systemd/system/multi-user.target.wants/proaudio-player-web.service"
 rm -f "$TARGET_DIR/usr/bin/proaudio-player-web"
+
+# DEV firmware: make SSH available immediately after networking comes up.
+if [ -f "$TARGET_DIR/usr/lib/systemd/system/sshd.service" ]; then
+	mkdir -p "$TARGET_DIR/etc/systemd/system/multi-user.target.wants"
+	ln -sf /usr/lib/systemd/system/sshd.service \
+		"$TARGET_DIR/etc/systemd/system/multi-user.target.wants/sshd.service"
+fi
