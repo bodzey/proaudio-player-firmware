a5fcdd0a18b3d2b8ca11b4728fb9ec84bd95406f
Bohdan Cherniak <70845142+bodzey@users.noreply.github.com>
2026-09-19 23:09:30 +0300

    firmware: colocate native runtime assets

---
+++ br2-external/package/proaudio-player-native/runtime/dlna-renderer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+interface="${DLNA_INTERFACE:-proaudio-dlna}"
+[ "$interface" != "lo" ] || {
+    echo "DLNA: loopback не підтримується libupnp" >&2
+    exit 1
+}
+[ -d "/sys/class/net/$interface" ] || {
+    echo "DLNA: внутрішній інтерфейс $interface не створено" >&2
+    exit 1
+}
+
+echo "DLNA: запуск transport worker на внутрішньому інтерфейсі $interface" >&2
+exec /usr/bin/gmediarender \
+    --interface-name="$interface" \
+    --port=49494 \
+    --friendly-name="ProAudio Player Internal Transport" \
+    --gstout-audiosink=pulsesink \
+    --gstout-initial-volume-db=0.0 \
+    --gstout-buffer-duration=0
